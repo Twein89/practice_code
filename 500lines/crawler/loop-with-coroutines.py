634483a48b0e865b3c0f4a82e06a9a5ae671bc06
Twein <twein1989@gmail.com>
2017-07-24 16:59:58 +0800

    try coroutines

--- 500lines/crawler/loop-with-coroutines.py
+++ 500lines/crawler/loop-with-coroutines.py
@@ -4,6 +4,12 @@ import re
 import urllib.parse
 import time
 
+urls_todo = set(['/'])
+seen_urls = set(['/'])
+concurrency_achieved = 0
+selector = DefaultSelector()
+stopped = False
+
 class Future:
 	def __init__(self):
 		self.result = None
@@ -25,4 +31,43 @@ def fetch(self):
 	except BlockingIOError:
 		pass
 
-	
+	f = Future()
+
+	def on_connected():
+		f.set_result(None)
+
+	selector.register(sock.fileno(),
+					  EVENT_WRITE,
+					  on_connected)
+	yield f
+	selector.unregister(sock.fileno())
+	print('connected!')
+
+
+class Task:
+	def __init__(self, coro):
+		self.coro = coro
+		f = Future()
+		f.set_result(None)
+		self.step(f)
+
+	def step(self, future):
+		try: 
+			next_future = self.coro.send(future.result)
+		except StopIteration:
+			return
+
+		next_future.add_done_callback(self.step)
+
+
+def loop():
+    while not stopped:
+        events = selector.select()
+        for event_key, event_mask in events:
+            callback = event_key.data
+            callback()
+
+fetcher = Fetcher('/353/')
+Task(fetcher.fetch())
+
+loop()
