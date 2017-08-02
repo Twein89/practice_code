86f8fb456166deafbe2d7ae989028d07f8900243
Twein <twein1989@gmail.com>
2017-08-02 15:00:13 +0800

    variadic arguments

--- click_tuto/click_tuto.py
+++ click_tuto/click_tuto.py
@@ -1,16 +1,11 @@
 import click
 
-def validate_rolls(ctx, param, value):
-    try:
-        rolls, dice = map(int, value.split('d', 2))
-        return (dice, rolls)
-    except ValueError:
-        raise click.BadParameter('rolls need to be in format Ndm')
-
 @click.command()
-@click.option('--rolls', callback=validate_rolls, default='1d6')
-def roll(rolls):
-    click.echo('Rolling a %d-sided dice %d time(s)' % rolls)
+@click.argument('src', nargs=-1)
+@click.argument('dst', nargs=1)
+def copy(src, dst):
+    print(src)
+    for fn in src:
+        click.echo('move %s to folder %s' % (fn, dst))
 
-if __name__ == '__main__':
-    roll()
+copy()
