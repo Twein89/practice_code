65e1c912b19b30ee11dc32fb46148c0d73a6867e
Twein <twein1989@gmail.com>
2017-08-02 10:31:25 +0800

    add click tuto

---
+++ click_tuto/click_tuto.py
@@ -0,0 +1,27 @@
+import click
+
+
+def print_version(ctx, param, value):
+    if not value or ctx.resilient_parsing:
+        return
+    click.echo('Version 1.0')
+    ctx.exit()
+
+
+
+@click.command()
+@click.option('--version', is_flag=True, callback=print_version, expose_value=False, is_eager=True)
+def hello():
+    click.echo('Hello World!')
+
+
+
+@click.command()
+@click.confirmation_option(help='Are you sure you want to drop the db?')
+def dropdb():
+    click.echo('Dropped all tables!')
+
+
+if __name__ == '__main__':
+    dropdb()
+    hello()
