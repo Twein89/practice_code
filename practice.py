95822e2cbcadb94d5a27b566f40e532225727a8a
Twein <twein1989@gmail.com>
2017-08-21 14:15:09 +0800

    depth first

--- practice.py
+++ practice.py
@@ -1,15 +1,31 @@
-def countdown(n):
-    print('Starting to count from', n)
-    while n > 0:
-        yield n
-        n -= 1
-    print('Done!')
+class Node:
+    def __init__(self, value):
+        self._value = value
+        self._children = []
 
-c = countdown(3)
-print(next(c))
+    def __repr__(self):
+        return 'Node({!r})'.format(self._value)
 
-print(next(c))
+    def add_child(self, node):
+        self._children.append(node)
 
-print(next(c))
+    def __iter__(self):
+        return iter(self._children)
 
-print(next(c))
+    def depth_first(self):
+        yield self
+        for c in self:
+            yield from c.depth_first()
+
+if __name__ == '__main__':
+    root = Node(0)
+    child1 = Node(1)
+    child2 = Node(2)
+    root.add_child(child1)
+    root.add_child(child2)
+    child1.add_child(Node(3))
+    child1.add_child(Node(4))
+    child2.add_child(Node(5))
+
+    for ch in root.depth_first():
+        print(ch)
