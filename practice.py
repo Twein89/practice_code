c01183291bfe3d8b9bfe0f1c60f90a6fdcefcb39
Twein <twein1989@gmail.com>
2017-08-25 21:27:02 +0800

    attach_wrapper

--- practice.py
+++ practice.py
@@ -1,28 +1,101 @@
+from functools import wraps, partial
 import time
-from functools import wraps
+import logging
+
 def timethis(func):
-    '''
-    Decorator that reports the execution time.
-    '''
+
     @wraps(func)
     def wrapper(*args, **kwargs):
         start = time.time()
         result = func(*args, **kwargs)
         end = time.time()
-        print(func.__name__, end-start)
+        print('countdown: ', end-start)
         return result
+
     return wrapper
 
+
+
+def attach_wrapper(obj, func=None):
+    if func is None:
+        return partial(attach_wrapper, obj)
+    setattr(obj, func.__name__, func)
+    return func
+
+def logged(level, name=None, message=None):
+
+    def decorate(func):
+        logname = name if name else func.__module__
+        log = logging.getLogger(logname)
+        logmsg = message if message else func.__name__
+
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            log.log(level, logmsg)
+            return func(*args, **kwargs)
+
+        @attach_wrapper(wrapper)
+        def set_level(newlevel):
+            nonlocal level
+            level = newlevel
+
+        @attach_wrapper(wrapper)
+        def set_message(newmsg):
+            nonlocal logmsg
+            logmsg = newmsg
+
+        # @attach_wrapper(wrapper)
+        # def get_level():
+        #     return level
+        wrapper.get_level = lambda: level
+        return wrapper
+    return decorate
+
+def logged_adj(level, name=None, message=None):
+    def decorate(func):
+        logname = name if name else func.__module__
+        log = logging.getLogger(logname)
+        logmsg = message if message else func.__name__
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            wrapper.log.log(wrapper.level, wrapper.logmsg)
+            return func(*args, **kwargs)
+
+        wrapper.level = level
+        wrapper.logmsg = logmsg
+        wrapper.log = log
+        return wrapper
+    return decorate
+
+@logged(logging.DEBUG)
+def add(x, y):
+    return x + y
+
+@logged(logging.CRITICAL, 'example')
+def spam():
+    print('Spam!')
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
+# print(add(2, 3))
+# add.set_message('Add called')
+# print(add(2, 3))
+# add.set_level(logging.WARNING)
+# print(add(2, 3))
+
 @timethis
-def countdown(n: int):
-    '''
-    Counts down
-    '''
+@logged_adj(logging.DEBUG)
+def countdown(n):
     while n > 0:
         n -= 1
 
 #countdown(1000000)
-#print(countdown.__name__)
-#print(countdown.__doc__)
-#print(countdown.__annotations__)
-print(countdown.__wrapped__)
+#countdown.set_level(logging.WARNING)
+#countdown.set_message('Counting down to zero')
+#print(countdown.get_level())
+#countdown(100000)
+
+countdown(1000000)
+#print(countdown.level)
+countdown.level = logging.WARNING
+countdown(1000000)
