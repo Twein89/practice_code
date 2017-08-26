6b3f6a3e62f2c3eebaf4b68f253bff45d53b8e8e
Twein <twein1989@gmail.com>
2017-08-27 00:59:03 +0800

    debug signature

--- practice.py
+++ practice.py
@@ -1,16 +1,22 @@
 from functools import wraps
 import inspect
 
+
 def optional_debug(func):
     if 'debug' in inspect.getargspec(func).args:
         raise TypeError('debug argument is already defined')
+
     @wraps(func)
     def wrapper(*args, debug=False, **kwargs):
         if debug:
             print('Calling: ', func.__name__)
         return func(*args, **kwargs)
     sig = inspect.signature(func)
-    parms = list(sig.parameters)
+    parms = list(sig.parameters.values())
+    parms.append(inspect.Parameter('debug',
+                                   inspect.Parameter.KEYWORD_ONLY,
+                                   default=False))
+    wrapper.__signature__ = sig.replace(parameters=parms)
     return wrapper
 
 # @optional_debug
@@ -19,6 +25,7 @@ def optional_debug(func):
 #
 # spam(1, 2, 3)
 # spam(1, 2, {'a': 1, 'b': 2}, debug=True)
+
 
 @optional_debug
 def add(x, y):
