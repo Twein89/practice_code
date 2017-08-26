f6accb0d64289b5998c62b8d17ea3b2da46d2faf
Twein <twein1989@gmail.com>
2017-08-26 19:41:34 +0800

    ..

--- practice.py
+++ practice.py
@@ -1,42 +1,36 @@
-import types
+import time
 from functools import wraps
 
-class Profiled:
-    def __init__(self, func):
-        wraps(func)(self)
-        self.ncalls = 0
-
-    def __call__(self, *args, **kwargs):
-        self.ncalls += 1
-        return self.__wrapped__(*args, **kwargs)
-
-    def __get__(self, instance, cls):
-        if instance is None:
-            return self
-        else:
-            return types.MethodType(self, instance)
-
-@Profiled
-def add(x, y):
-    return x + y
+def timethis(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        r = func(*args, **kwargs)
+        end = time.time()
+        print('timing: ', end - start)
+        return r
+    return wrapper
 
 class Spam:
-    @Profiled
-    def bar(self, x):
-        print(self, x)
+    @timethis
+    def instance_method(self, n):
+        print(self, n)
+        while n > 0:
+            n -= 1
 
-# print(add(1, 2))
-# print(add(2, 3))
-# print(add.ncalls)
-#
-# s = Spam()
-# s.bar(1)
-# s.bar(2)
-# s.bar(3)
-# print(Spam.bar.ncalls)
+    @classmethod
+    @timethis
+    def class_method(cls, n):
+        print(cls, n)
+        while n > 0:
+            n -= 1
+
+    @staticmethod
+    @timethis
+    def static_method(n):
+        print(n)
+        while n > 0:
+            n -= 1
 
 s = Spam()
-def grok(self, x):
-    pass
-
-print(grok.__get__(s, Spam))
+s.instance_method(1000000)
