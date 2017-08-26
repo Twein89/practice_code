cb1c51fef529b84e77c39b95edf07db629f5c7ac
Twein <twein1989@gmail.com>
2017-08-26 23:29:46 +0800

    ..

--- practice.py
+++ practice.py
@@ -1,36 +1,27 @@
-import time
 from functools import wraps
+import inspect
 
-def timethis(func):
+def optional_debug(func):
+    if 'debug' in inspect.getargspec(func).args:
+        raise TypeError('debug argument is already defined')
     @wraps(func)
-    def wrapper(*args, **kwargs):
-        start = time.time()
-        r = func(*args, **kwargs)
-        end = time.time()
-        print('timing: ', end - start)
-        return r
+    def wrapper(*args, debug=False, **kwargs):
+        if debug:
+            print('Calling: ', func.__name__)
+        return func(*args, **kwargs)
+    sig = inspect.signature(func)
+    parms = list(sig.parameters)
     return wrapper
 
-class Spam:
-    @timethis
-    def instance_method(self, n):
-        print(self, n)
-        while n > 0:
-            n -= 1
+# @optional_debug
+# def spam(x, y, z):
+#     print(x, y, z)
+#
+# spam(1, 2, 3)
+# spam(1, 2, {'a': 1, 'b': 2}, debug=True)
 
-    @classmethod
-    @timethis
-    def class_method(cls, n):
-        print(cls, n)
-        while n > 0:
-            n -= 1
+@optional_debug
+def add(x, y):
+    return x + y
 
-    @staticmethod
-    @timethis
-    def static_method(n):
-        print(n)
-        while n > 0:
-            n -= 1
-
-s = Spam()
-s.instance_method(1000000)
+print(inspect.signature(add))
