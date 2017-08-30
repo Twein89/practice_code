1b2daf2c3503e1412ec38c00aaa5fc231a98e233
Twein <twein1989@gmail.com>
2017-08-30 15:45:05 +0800

    signature

--- practice.py
+++ practice.py
@@ -1,80 +1,49 @@
-from collections import OrderedDict
+from inspect import Signature, Parameter
 
-class Typed:
-    _expected_type = type(None)
-    def __init__(self, name=None):
-        self._name = name
+def make_sig(*names):
+    parms = [Parameter(name, Parameter.POSITIONAL_OR_KEYWORD)
+            for name in names]
+    return Signature(parms)
 
-    def __set__(self, instance, value):
-        if not isinstance(value, self._expected_type):
-            raise TypeError('Expected ' + str(self._expected_type))
-        instance.__dict__[self._name] = value
+# class Structure:
+#     __signature__ = make_sig()
+#     def __init__(self, *args, **kwargs):
+#         bound_values = self.__signature__.bind(*args, **kwargs)
+#         for name, value in bound_values.arguments.items():
+#             setattr(self, name, value)
+#
+# class Stock(Structure):
+#     __signature__ = make_sig('name', 'shares', 'price')
+#
+# class Point(Structure):
+#     __signature__ = make_sig('x', 'y')
+#
+# import inspect
+# print(inspect.signature(Stock))
+#
+# s1 = Stock('ACME', 100, 490.1)
+# # s2 = Stock('ACME', 100)
+# s3 = Stock('ACME', 100, 490.1, shares=50)
 
-class Integer(Typed):
-    _expected_type = int
-
-class Float(Typed):
-    _expected_type = float
-
-class String(Typed):
-    _expected_type = str
-
-class OrderedMeta(type):
+class StructureMeta(type):
     def __new__(cls, clsname, bases, clsdict):
-        d = dict(clsdict)
-        order = []
-        for name, value in clsdict.items():
-            if isinstance(value, Typed):
-                value._name = name
-                order.append(name)
-        d['_order'] = order
-        return type.__new__(cls, clsname, bases, d)
+        clsdict['__signature__'] = make_sig(*clsdict.get('_fields', []))
+        return super().__new__(cls, clsname, bases, clsdict)
 
-    @classmethod
-    def __prepare__(cls, clsname, bases):
-        return OrderedDict()
-
-class Structure(metaclass=OrderedMeta):
-    def as_csv(self):
-        return ','.join(str(getattr(self, name)) for name in self._order)
+class Structure(metaclass=StructureMeta):
+    _fields = []
+    def __init__(self, *args, **kwargs):
+        bound_values = self.__signature__.bind(*args, **kwargs)
+        for name, value in bound_values.arguments.items():
+            setattr(self, name, value)
 
 class Stock(Structure):
-    name = String()
-    shares = Integer()
-    price = Float()
+    _fields = ['name', 'shares', 'price']
 
-    def __init__(self, name, shares, price):
-        self.name = name
-        self.shares = shares
-        self.price = price
+class Point(Structure):
+    _fields = ['x', 'y']
 
+import inspect
 
-# s = Stock('GOOG', 100, 490.1)
-# print(s.name)
-# print(s.as_csv())
-# t = Stock('AAPL', 'a lot', 610.23)
-
-class NoDupOrderedDict(OrderedDict):
-    def __init__(self, clsname):
-        self.clsname = clsname
-        super().__init__()
-    def __setitem__(self, name, value):
-        if name in self:
-            raise TypeError('{} already defined in {}'.format(name, self.clsname))
-        super().__setitem__(name, value)
-
-class OrderedMeta(type):
-    def __new__(cls, clsname, bases, clsdict):
-        d = dict(clsdict)
-        d['_order'] = [name for name in clsdict if name[0] != '_']
-        return type.__new__(cls, clsname, bases, d)
-
-    @classmethod
-    def __prepare__(cls, clsname, bases):
-        return NoDupOrderedDict(clsname)
-
-class A(metaclass=OrderedMeta):
-    def spam(self):
-        pass
-    def spam(self):
-        pass
+print(inspect.signature(Stock))
+print(inspect.signature(Point))
