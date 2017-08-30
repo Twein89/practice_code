f221b0dee745774067d483e01f252a96d8994b2b
Twein <twein1989@gmail.com>
2017-08-30 14:04:00 +0800

    orderedmeta

--- practice.py
+++ practice.py
@@ -1,26 +1,80 @@
-import weakref
+from collections import OrderedDict
 
-class Cached(type):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.__cache = weakref.WeakValueDictionary()
+class Typed:
+    _expected_type = type(None)
+    def __init__(self, name=None):
+        self._name = name
 
-    def __call__(self, *args):
-        if args in self.__cache:
-            return self.__cache[args]
-        else:
-            obj = super().__call__(*args)
-            self.__cache[args] = obj
-            return obj
+    def __set__(self, instance, value):
+        if not isinstance(value, self._expected_type):
+            raise TypeError('Expected ' + str(self._expected_type))
+        instance.__dict__[self._name] = value
 
-class Spam(metaclass=Cached):
-    def __init__(self, name):
-        print('Creating Spam({!r})'.format(name))
+class Integer(Typed):
+    _expected_type = int
+
+class Float(Typed):
+    _expected_type = float
+
+class String(Typed):
+    _expected_type = str
+
+class OrderedMeta(type):
+    def __new__(cls, clsname, bases, clsdict):
+        d = dict(clsdict)
+        order = []
+        for name, value in clsdict.items():
+            if isinstance(value, Typed):
+                value._name = name
+                order.append(name)
+        d['_order'] = order
+        return type.__new__(cls, clsname, bases, d)
+
+    @classmethod
+    def __prepare__(cls, clsname, bases):
+        return OrderedDict()
+
+class Structure(metaclass=OrderedMeta):
+    def as_csv(self):
+        return ','.join(str(getattr(self, name)) for name in self._order)
+
+class Stock(Structure):
+    name = String()
+    shares = Integer()
+    price = Float()
+
+    def __init__(self, name, shares, price):
         self.name = name
+        self.shares = shares
+        self.price = price
 
-a = Spam('Guido')
-b = Spam('Diana')
-c = Spam('Guido')
 
-print(a is b)
-print(a is c)
+# s = Stock('GOOG', 100, 490.1)
+# print(s.name)
+# print(s.as_csv())
+# t = Stock('AAPL', 'a lot', 610.23)
+
+class NoDupOrderedDict(OrderedDict):
+    def __init__(self, clsname):
+        self.clsname = clsname
+        super().__init__()
+    def __setitem__(self, name, value):
+        if name in self:
+            raise TypeError('{} already defined in {}'.format(name, self.clsname))
+        super().__setitem__(name, value)
+
+class OrderedMeta(type):
+    def __new__(cls, clsname, bases, clsdict):
+        d = dict(clsdict)
+        d['_order'] = [name for name in clsdict if name[0] != '_']
+        return type.__new__(cls, clsname, bases, d)
+
+    @classmethod
+    def __prepare__(cls, clsname, bases):
+        return NoDupOrderedDict(clsname)
+
+class A(metaclass=OrderedMeta):
+    def spam(self):
+        pass
+    def spam(self):
+        pass
