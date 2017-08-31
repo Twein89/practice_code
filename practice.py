2981917b636354d81f64d082ea2bd4e3009aebb0
Twein <twein1989@gmail.com>
2017-08-31 18:06:00 +0800

    visitor

--- practice.py
+++ practice.py
@@ -1,79 +1,106 @@
-class LoggedMappingMixin:
-    __slots__ = ()
-
-    def __getitem__(self, key):
-        print('Getting ' + str(key))
-        return super().__getitem__(key)
-
-    def __setitem__(self, key, value):
-        print('Setting {} = {!r}'.format(key, value))
-        return super().__setitem__(key, value)
-
-    def __delitem__(self, key):
-        print('Deleting ' + str(key))
-        return super().__delitem__(key)
-
-class SetOnceMappingMixin:
-    __slots__ = ()
-
-    def __setitem__(self, key, value):
-        if key in self:
-            raise KeyError(str(key) + ' already set')
-        return super().__setitem__(key, value)
-
-class StringKeysMappingMixin:
-    __slots__ = ()
-
-    def __setitem__(self, key, value):
-        if not isinstance(key, str):
-            raise TypeError('keys must be strings')
-        return super().__setitem__(key, value)
-
-# class LoggedDict(LoggedMappingMixin, dict):
-#     pass
-#
-# d = LoggedDict()
-# d['x'] = 23
-# print(d['x'])
-# del d['x']
-
-from collections import defaultdict
-
-class SetOnceDefaultDict(SetOnceMappingMixin, defaultdict):
+class Node:
     pass
 
-# d = SetOnceDefaultDict(list)
-# d['x'].append(2)
-# d['x'].append(3)
-# print(d['x'])
-# d['x'] = 23
+class UnaryOperator(Node):
+    def __init__(self, operand):
+        self.operand = operand
 
-def LoggedMapping(cls):
-    cls_getitem = cls.__getitem__
-    cls_setitem = cls.__setitem__
-    cls_delitem = cls.__delitem__
+class BinaryOperator(Node):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
 
-    def __getitem__(self, key):
-        print('Getting ' + str(key))
-        return cls_getitem(self, key)
-
-    def __setitem__(self, key, value):
-        print('Setting {} = {!r}'.format(key, value))
-        return cls_setitem(self, key, value)
-
-    def __delitem__(self, key):
-        print('Deleting ' + str(key))
-        return cls_delitem(self, key)
-    cls.__getitem__ = __getitem__
-    cls.__setitem__ = __setitem__
-    cls.__delitem__ = __delitem__
-    return cls
-
-@LoggedMapping
-class LoggedDict(dict):
+class Add(BinaryOperator):
     pass
 
-d = LoggedDict()
-d['x'] = 23
-print(d['x'])
-del d['x']
+class Sub(BinaryOperator):
+    pass
+
+class Mul(BinaryOperator):
+    pass
+
+class Div(BinaryOperator):
+    pass
+
+class Negate(UnaryOperator):
+    pass
+
+class Number(Node):
+    def __init__(self, value):
+        self.value = value
+
+t1 = Sub(Number(3), Number(4))
+t2 = Mul(Number(2), t1)
+t3 = Div(t2, Number(5))
+t4 = Add(Number(1), t3)
+
+class NodeVisitor:
+    def visit(self, node):
+        methname = 'visit_' + type(node).__name__
+        meth = getattr(self, methname, None)
+        if meth is None:
+            meth = self.generic_visit
+        return meth(node)
+
+    def generic_visit(self, node):
+        raise RuntimeError('No {} method'.format('visit_' + type(node).__name__))
+
+class Evaluator(NodeVisitor):
+    def visit_Number(self, node):
+        return node.value
+
+    def visit_Add(self, node):
+        return self.visit(node.left) + self.visit(node.right)
+
+    def visit_Sub(self, node):
+        return self.visit(node.left) - self.visit(node.right)
+
+    def visit_Mul(self, node):
+        return self.visit(node.left) * self.visit(node.right)
+
+    def visit_Div(self, node):
+        return self.visit(node.left) / self.visit(node.right)
+
+    def visit_Negate(self, node):
+        return -node.operand
+
+# e = Evaluator()
+# r = e.visit(t4)
+# int(r)
+
+class StackCode(NodeVisitor):
+    def generate_code(self, node):
+        self.instructions = []
+        self.visit(node)
+        return self.instructions
+
+    def visit_Number(self, node):
+        self.instructions.append(('PUSH', node.value))
+
+    def binop(self, node, instruction):
+        self.visit(node.left)
+        self.visit(node.right)
+        self.instructions.append((instruction,))
+
+    def visit_Add(self, node):
+        self.binop(node, 'ADD')
+
+    def visit_Sub(self, node):
+        self.binop(node, 'SUB')
+
+    def visit_Mul(self, node):
+        self.binop(node, 'MUL')
+
+    def visit_Div(self, node):
+        self.binop(node, 'DIV')
+
+    def unaryop(self, node, instruction):
+        self.visit(node.operand)
+        self.instructions.append((instruction,))
+
+    def visit_Negate(self, node):
+        self.unargop(node, 'NEG')
+
+s = StackCode()
+r = s.generate_code(t4)
+print(r)
