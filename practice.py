e68c3ce2242023c26062d3bc271127e51ef7f598
Twein <twein1989@gmail.com>
2017-08-31 11:10:22 +0800

    multimethod

--- practice.py
+++ practice.py
@@ -1,27 +1,103 @@
-import operator
+import inspect
+import types
 
-class StructTupleMeta(type):
-    def __init__(cls, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for n, name in enumerate(cls._fields):
-            setattr(cls, name, property(operator.itemgetter(n)))
 
-class StructTuple(tuple, metaclass=StructTupleMeta):
-    _fields = []
-    def __new__(cls, *args):
-        if len(args) != len(cls._fields):
-            raise ValueError('{} arguments required'.format(len(cls._fields)))
-        return super().__new__(cls, args)
+class MultiMethod:
 
-class Stock(StructTuple):
-    _fields = ['name', 'shares', 'price']
+    def __init__(self, name):
+        self._methods = {}
+        self.__name__ = name
 
-class Point(StructTuple):
-    _fields = ['x', 'y']
+    def register(self, meth):
+        sig = inspect.signature(meth)
+        types = []
+        for name, parm in sig.parameters.items():
+            #print('registed', name, parm)
+            if name == 'self':
+                continue
+            if parm.annotation is inspect.Parameter.empty:
+                raise TypeError(
+                    'Argument {} must be annotated with a type'.format(name)
+                )
+            if not isinstance(parm.annotation, type):
+                raise TypeError(
+                    'Argument {} annotation must be a type'.format(name)
+                )
+            if parm.default is not inspect.Parameter.empty:
+                self._methods[tuple(types)] = meth
+            types.append(parm.annotation)
+        self._methods[tuple(types)] = meth
 
-s = Stock('ACME', 50, 91.1)
-print(s)
-print(s[0])
-print(s.name)
-print(s.shares * s.price)
-s.shares = 23
+    def __call__(self, *args):
+        types = tuple(type(arg) for arg in args[1:])
+        meth = self._methods.get(types, None)
+        if meth:
+            return meth(*args)
+        else:
+            raise TypeError('No matching method for types {}'.format(types))
+
+    def __get__(self, instance, cls):
+        if instance is not None:
+            return types.MethodType(self, instance)
+        else:
+            return self
+
+
+class MultiDict(dict):
+
+    def __setitem__(self, key, value):
+        if key in self:
+            current_value = self[key]
+            if isinstance(current_value, MultiMethod):
+                current_value.register(value)
+            else:
+                mvalue = MultiMethod(key)
+                mvalue.register(current_value)
+                mvalue.register(value)
+                super().__setitem__(key, mvalue)
+        else:
+            super().__setitem__(key, value)
+
+
+class MultipleMeta(type):
+
+    def __new__(cls, clsname, bases, clsdict):
+        return type.__new__(cls, clsname, bases, dict(clsdict))
+
+    @classmethod
+    def __prepare__(cls, clsname, bases):
+        return MultiDict()
+
+
+class Spam(metaclass=MultipleMeta):
+
+    def bar(self, x: int, y: int):
+        print('Bar 1:', x, y)
+
+    def bar(self, s: str, n: int = 0):
+        print('Bar 2:', s, n)
+
+import time
+
+
+class Date(metaclass=MultipleMeta):
+
+    def __init__(self, year: int, month: int, day: int):
+        self.year = year
+        self.month = month
+        self.day = day
+
+    def __init__(self):
+        t = time.localtime()
+        self.__init__(t.tm_year, t.tm_mon, t.tm_mday)
+
+
+s = Spam()
+s.bar(2, 3)
+s.bar('hello')
+s.bar('hello', 5)
+d = Date(2012, 12, 21)
+print(d.year)
+e = Date()
+print(e.year)
+
