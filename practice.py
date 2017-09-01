8e3dc8345101be9aa2e02eba924c85104129bb38
Twein <twein1989@gmail.com>
2017-09-01 11:20:41 +0800

    visitor pattern without Recursion

--- practice.py
+++ practice.py
@@ -1,5 +1,44 @@
+import types
+
 class Node:
     pass
+
+class NodeVisitor:
+    def visit(self, node):
+        stack = [node]
+        last_result = None
+        while stack:
+            try:
+                last = stack[-1]
+                print('before: ', stack)
+                if isinstance(last, types.GeneratorType):
+                    print('1')
+                    # stack.append(next(last))
+                    stack.append(last.send(last_result))
+                    last_result = None
+                elif isinstance(last, Node):
+                    print('2')
+                    stack.append(self._visit(stack.pop()))
+                else:
+                    print('3')
+                    last_result = stack.pop()
+
+                print('after: ', stack)
+            except StopIteration:
+                stack.pop()
+            print('---------------')
+
+        return last_result
+
+    def _visit(self, node):
+        methname = 'visit_' + type(node).__name__
+        meth = getattr(self, methname, None)
+        if meth is None:
+            meth = self.generic_visit
+        return meth(node)
+
+    def generic_visit(self, node):
+        raise RuntimeError('No {} method'.format('visit_' + type(node).__name__))
 
 class UnaryOperator(Node):
     def __init__(self, operand):
@@ -29,78 +68,35 @@ class Number(Node):
     def __init__(self, value):
         self.value = value
 
-t1 = Sub(Number(3), Number(4))
-t2 = Mul(Number(2), t1)
-t3 = Div(t2, Number(5))
-t4 = Add(Number(1), t3)
-
-class NodeVisitor:
-    def visit(self, node):
-        methname = 'visit_' + type(node).__name__
-        meth = getattr(self, methname, None)
-        if meth is None:
-            meth = self.generic_visit
-        return meth(node)
-
-    def generic_visit(self, node):
-        raise RuntimeError('No {} method'.format('visit_' + type(node).__name__))
-
 class Evaluator(NodeVisitor):
     def visit_Number(self, node):
         return node.value
 
     def visit_Add(self, node):
-        return self.visit(node.left) + self.visit(node.right)
+        yield (yield node.left) + (yield node.right)
 
     def visit_Sub(self, node):
-        return self.visit(node.left) - self.visit(node.right)
+        yield (yield node.left) - (yield node.right)
 
     def visit_Mul(self, node):
-        return self.visit(node.left) * self.visit(node.right)
+        yield (yield node.left) * (yield node.right)
 
     def visit_Div(self, node):
-        return self.visit(node.left) / self.visit(node.right)
+        yield (yield node.left) / (yield node.right)
 
     def visit_Negate(self, node):
-        return -node.operand
+        yield (yield node.operand)
 
-# e = Evaluator()
-# r = e.visit(t4)
-# int(r)
+if __name__ == '__main__':
+    # t1 = Sub(Number(3), Number(4))
+    # t2 = Mul(Number(2), t1)
+    # t3 = Div(t2, Number(5))
+    # t4 = Add(Number(1), t3)
+    # e = Evaluator()
+    # print(e.visit(t4))
+    a = Number(0)
+    for n in range(1, 3):
+        a = Add(a, Number(n))
 
-class StackCode(NodeVisitor):
-    def generate_code(self, node):
-        self.instructions = []
-        self.visit(node)
-        return self.instructions
-
-    def visit_Number(self, node):
-        self.instructions.append(('PUSH', node.value))
-
-    def binop(self, node, instruction):
-        self.visit(node.left)
-        self.visit(node.right)
-        self.instructions.append((instruction,))
-
-    def visit_Add(self, node):
-        self.binop(node, 'ADD')
-
-    def visit_Sub(self, node):
-        self.binop(node, 'SUB')
-
-    def visit_Mul(self, node):
-        self.binop(node, 'MUL')
-
-    def visit_Div(self, node):
-        self.binop(node, 'DIV')
-
-    def unaryop(self, node, instruction):
-        self.visit(node.operand)
-        self.instructions.append((instruction,))
-
-    def visit_Negate(self, node):
-        self.unargop(node, 'NEG')
-
-s = StackCode()
-r = s.generate_code(t4)
-print(r)
+    e = Evaluator()
+    print(e.visit(a))
