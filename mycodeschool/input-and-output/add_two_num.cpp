2a32adb22aa78d1ae5c4245c240886a9da9c72a8
Twein <twein1989@gmail.com>
2017-09-09 05:50:45 +0800

    add two num

---
+++ mycodeschool/input-and-output/add_two_num.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cstdio>
+#include<cmath>
+
+using namespace std;
+
+int main() {
+    int num1, num2;
+    cin >> num1 >> num2;
+    int sum = (num1 + num2);
+    cout << sum << endl;
+    return 0;
+}
