84e9ee207c99d516d5109eeb8f221829978c052c
Twein <twein1989@gmail.com>
2017-09-14 07:18:26 +0800

    insert Node at nth position

--- mycodeschool/linked-list/insertAtNth.cpp
+++ mycodeschool/linked-list/insertAtNth.cpp
@@ -17,22 +17,42 @@ void Print(Node *head) {
 }
 
 Node* Insert(Node *head, int data) {
-    Node* new_head = new Node();
-    new_head -> data = data;
-    new_head -> next = head;
-    return new_head;
+    Node* temp = new Node();
+    temp -> data = data;
+    temp -> next = head;
+    return temp;
 }
 
 Node* InsertNth(Node *head, int data, int n) {
+    Node* temp = new Node();
+    temp -> data = data;
+    temp -> next = NULL;
     if (n == 1) {
-
+        temp -> next = head;    
+        return temp;
     }
+    Node* iter = head; 
+    for (int i=1; i<n-1; i++) {
+        iter = iter -> next;
+    }
+    if(iter -> next != NULL) {
+        Node* after = iter -> next;  
+        iter -> next = temp;
+        temp -> next = after;
+    }
+    else {
+        iter -> next = temp;
+    }
+    return head;
 }
 
 int main() {
     Node* B = new Node();
     B -> data = 5;
-    B = Insert(B, 3);
+    B = InsertNth(B, 3, 1);
+    B = InsertNth(B, 88, 2);
+    B = InsertNth(B, 23, 3);
+    B = InsertNth(B, 110, 5);
     Print(B);
     return 0;
 }
