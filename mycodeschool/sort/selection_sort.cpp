8c385acfd99db82e2b56a2226a12a68f56a814b7
Twein <twein1989@gmail.com>
2017-11-02 21:18:18 +0800

    add sort

---
+++ mycodeschool/sort/selection_sort.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+void SelectionSort(int arr[], int n) {
+    for(int i=0; i < n; i++) {
+        
+    } 
+}
+
+int main() {
+    int arr[] = {2, 7, 3, 1, 8, 21};
+    int n = 6;
+    //int test_cases;
+    //cin>>test_cases;
+    //while(test_cases--) {
+    //    int n;
+    //    cin >> n;
+    //    int arr[n];
+    //    for(int i=0; i < n; i++) cin>>arr[i];
+    //    cout << n << endl;
+    //    for(int i=0; i < n; i++) cout<<arr[i]<<' ';
+    //    cout << endl;
+    //}
+}
