f8f07e41ecfc310dbd077c05db609d152133d961
Twein <twein1989@gmail.com>
2018-01-11 15:50:20 +0800

    click git clone

--- click_tuto/hello.py
+++ click_tuto/hello.py
@@ -1,11 +1,27 @@
 import click
+import os
 
-@click.command()
-@click.argument('src', nargs=-1)
-@click.argument('dst', nargs=1)
-def copy(src, dst):
-    for fn in src:
-        click.echo('move %s to folder %s' % (fn, dst))
+class Repo(object):
+    def __init__(self, home=None, debug=False):
+        self.home = os.path.abspath(home or '.')
+        self.debug = debug
+
+@click.group()
+@click.option('--repo-home', envvar='REPO_HOME', default='.repo')
+@click.option('--debug/--no-debug', default=False,
+              envvar='REPO_DEBUG')
+@click.pass_context
+def cli(ctx, repo_home, debug):
+    ctx.obj = Repo(repo_home, debug)
+
+
+@cli.command()
+@click.argument('src')
+@click.argument('dest', required=False)
+@click.pass_obj
+def clone(repo, src, dest):
+    print(repo.home)
+    print(repo.debug)
 
 if __name__ == '__main__':
-    copy()
+    cli()
