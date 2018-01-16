76adfcbae37776e9e5b60cb91ca4e7bccf29854a
Twein <twein1989@gmail.com>
2018-01-16 09:16:05 +0800

    functools partial

--- click_tuto/hello.py
+++ click_tuto/hello.py
@@ -1,27 +1,11 @@
 import click
 import os
 
-class Repo(object):
-    def __init__(self, home=None, debug=False):
-        self.home = os.path.abspath(home or '.')
-        self.debug = debug
-
-@click.group()
-@click.option('--repo-home', envvar='REPO_HOME', default='.repo')
-@click.option('--debug/--no-debug', default=False,
-              envvar='REPO_DEBUG')
-@click.pass_context
-def cli(ctx, repo_home, debug):
-    ctx.obj = Repo(repo_home, debug)
-
-
-@cli.command()
-@click.argument('src')
-@click.argument('dest', required=False)
-@click.pass_obj
-def clone(repo, src, dest):
-    print(repo.home)
-    print(repo.debug)
+@click.command()
+@click.option('--hash-type', type=click.Choice(['md5', 'sha1']))
+def digest(hash_type):
+    print(hash_type)
+    click.echo(hash_type)
 
 if __name__ == '__main__':
-    cli()
+    digest()
