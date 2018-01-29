0f7112541aa01c20a674127614685a78fff4054a
Twein <twein1989@gmail.com>
2018-01-29 19:57:50 +0800

    ...

---
+++ leetcode/tree/path-sum.py
@@ -0,0 +1,48 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def hasPathSum(self, root, sum):
+        pass
+    def travel(self, root):
+        if root is None:
+            return []
+        result, current = [], [root]
+        while current:
+            vals, next_level = [], []
+            for node in current:
+                vals.append(node.val)
+                if node.left is not None:
+                    next_level.append(node.left)
+                if node.right is not None:
+                    next_level.append(node.right)
+            current = next_level
+            print(vals)
+
+
+
+
+
+if __name__ == '__main__':
+    n0 = TreeNode(5)
+    n1 = TreeNode(4)
+    n2 = TreeNode(8)
+    n0.left = n1
+    n0.right = n2
+    n3 = TreeNode(11)
+    n4 = TreeNode(13)
+    n5 = TreeNode(4)
+    n1.left = n3
+    n2.left = n4
+    n2.right = n5
+    n6 = TreeNode(7)
+    n7 = TreeNode(2)
+    n8 = TreeNode(1)
+    n3.left = n6
+    n3.right = n7
+    n5.right = n8
+    s = Solution()
+    s.travel(n0)
