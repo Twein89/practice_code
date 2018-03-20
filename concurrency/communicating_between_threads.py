e8a3ed6c97c442366b3f15f1f6c1f6da036785a9
Twein <twein1989@gmail.com>
2018-03-20 14:00:11 +0800

    wordcount

---
+++ concurrency/communicating_between_threads.py
@@ -0,0 +1,16 @@
+from queue import Queue
+from threading import Thread
+
+def producer(out_q):
+    while True:
+        out_q.put(data)
+
+def consumer(in_q):
+    while True:
+        data = in_q.get()
+
+q = Queue()
+t1 = Thread(target=consumer, args=(q,))
+t2 = Thread(target=producer, args=(q,))
+t1.start()
+t2.start()
